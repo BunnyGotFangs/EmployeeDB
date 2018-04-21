ed6e6c8349bfda764178f609cec89fd18c2fc4ed
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-04-21 13:25:48 -0500

    text fields change

--- empDB.js
+++ empDB.js
@@ -1,52 +1,42 @@
 
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBctsXYC084rpKqaYgoFazLhACkNvlb-gw",
-    authDomain: "employeedb-d2911.firebaseapp.com",
-    databaseURL: "https://employeedb-d2911.firebaseio.com",
-    projectId: "employeedb-d2911",
-    storageBucket: "",
-    messagingSenderId: "894274672549"
-  };
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBctsXYC084rpKqaYgoFazLhACkNvlb-gw",
+  authDomain: "employeedb-d2911.firebaseapp.com",
+  databaseURL: "https://employeedb-d2911.firebaseio.com",
+  projectId: "employeedb-d2911",
+  storageBucket: "",
+  messagingSenderId: "894274672549"
+};
 
 
-  firebase.initializeApp(config);
-  var database=firebase.database();
+firebase.initializeApp(config);
+var database = firebase.database();
 
-  var name = "";
-  var role ="";
-  var startDate ="";
-  var monthlyRate ="";
+var name = "";
+var role = "";
+var startDate = "";
+var monthlyRate = "";
 
-  $("#submitBtn").on("click", function(event) {
-    // Prevent the page from refreshing
-    event.preventDefault();
+$("#submitBtn").on("click", function (event) {
+  // Prevent the page from refreshing
+  event.preventDefault();
 
-    // Get inputs
-    name = $("#name-input").val().trim();
-    role = $("#role-input").val().trim();
-    startDate = $("#date-input").val().trim();
-    monthlyRate = $("#rate-input").val().trim();
+  // Get inputs
+  name = $("#name-input").val().trim();
+  role = $("#role-input").val().trim();
+  startDate = $("#date-input").val().trim();
+  monthlyRate = $("#rate-input").val().trim();
 
   database.ref().push({
 
-  name: name,
-  role: role,
-  startDate: startDate,
-  monthlyRate: monthlyRate
+    name: name,
+    role: role,
+    startDate: startDate,
+    monthlyRate: monthlyRate,
+  });
 });
-})
-
-
-  //database.ref().on("value", function(snapshot) {
-  //$("#displayed-data").text(snapshot.val().name + " | " + snapshot.val()//.role + " | " + snapshot.val().startDate)+ " | " + snapshot.val()//.monthlyRate)}
 
 
 
- //database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
-
-  // database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
-  //  $("name-display").text(snapshot.val().name);
-  // });
-  
