896934f927245441b937932d106766862bbf48f4
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-04-21 11:23:59 -0500

    added JS file

---
+++ empDB.js
@@ -0,0 +1,12 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBctsXYC084rpKqaYgoFazLhACkNvlb-gw",
+    authDomain: "employeedb-d2911.firebaseapp.com",
+    databaseURL: "https://employeedb-d2911.firebaseio.com",
+    projectId: "employeedb-d2911",
+    storageBucket: "",
+    messagingSenderId: "894274672549"
+  };
+  firebase.initializeApp(config);
