82716945d643d20cafc043748773cf1d4b2ccb53
BunnygotFangs <kimberly@thehilts.com>
2018-04-21 11:56:46 -0500

    update of JS to capture input and place in Firebase going to share comment and testing

--- empDB.js
+++ empDB.js
@@ -9,4 +9,36 @@
     storageBucket: "",
     messagingSenderId: "894274672549"
   };
+
+
   firebase.initializeApp(config);
+  var database=firebase.database();
+
+  var name = "";
+  var role ="";
+  var startDate ="";
+  var monthlyRate ="";
+
+  $("#submitBtn").on("click", function(event) {
+    // Prevent the page from refreshing
+    event.preventDefault();
+
+    // Get inputs
+    name = $("#name-info").val().trim();
+    role = $("#role-info").val().trim();
+    startDate = $("#date-info").val().trim();
+    monthlyRate = $("#rate-info").val().trim();
+
+  database.ref().push({
+
+  name: name,
+  role: role,
+  startDate: startDate,
+  monthlyRate: monthlyRate});
+})
+
+
+
+  database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
+    $("name-display").text(snapshot.val().name);
+  });
